becc14bc740d0f56760fa569761ba1748d3531cf
cmt218 <cmt218@lehigh.edu>
2016-01-29 16:46:26 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Cole Tomlinson
+//Lab 1
+//Hello World Java Program
+
+//compile and run
+
+//define class
+public class HelloWorld{
+    
+//add main method
+    public static void main(String[] args){
+        
+//print statement
+        System.out.println("####################");
+        System.out.println("####################");
+        System.out.println("#                  #");
+        System.out.println("# Hello World 2014 #");
+        System.out.println("#                  #");
+        System.out.println("####################");
+        System.out.println("####################");
+        System.out.println("test");
+        
+    }
+}
